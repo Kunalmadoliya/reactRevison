112573975f3da62447cadf513151239f78a4c8fe
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-07-10 15:50:03 +0530

    10-07-25

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth";
+
+import {login, logout} from "./store/authSlice";
 
 const App = () => {
   console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return <div>hi</div>;
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}));
+        } else {
+          dispatch(logout({userData}));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? <div>hi</div> : null;
 };
 
 export default App;
